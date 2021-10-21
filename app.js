4cf1575b1860f66ec292081a52391a068d625c87
Roman TIMAKOV <timakov_r@M11highway.pro>
2021-10-21 11:20:59 +0300

    check

--- app.js
+++ app.js
@@ -9,8 +9,15 @@ const ErrorsAll = require('./middlewares/Errors');
 const router = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { DATA_BASE, PORT } = require('./utils/ConfigEnv');
+const { limiter } = require('./middlewares/limiter');
+const { corsOptions } = require('./utils/constans');
 
 const app = express();
+app.use(cors(corsOptions));
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(DATA_BASE, {
   useNewUrlParser: true,
@@ -19,21 +26,9 @@ mongoose.connect(DATA_BASE, {
   useFindAndModify: false,
 });
 
-app.use('*', cors({
-  origin: ['https://agroball.diplom.nomoredomains.monster', 'http://localhost:3000'],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'authorization'],
-  credentials: true,
-}));
-app.use(helmet());
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(requestLogger);
 
+app.use(limiter);
 app.use('/', router);
 app.use(errorLogger);
 
---
+++ utils/constans.js
@@ -0,0 +1,8 @@
+const corsOptions = {
+  origin: ['https://agroball.diplom.nomoredomains.monster', 'http://localhost:3000'],
+  credentials: true,
+};
+
+module.exports = {
+  corsOptions,
+};
