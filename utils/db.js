c2282e5a4cff651767d369a23ded39f1632acc6e
Agroball <rotimQW@gmail.com>
2021-11-16 20:01:32 +0300

    1

--- utils/db.js
+++ utils/db.js
@@ -30,7 +30,7 @@ module.exports.movieIdNotFound = 'Фильм с указанным _id не на
 
 module.exports.corsOptions = {
   origin: [
-    'https://api.agroball.diplom.nomoredomains.monster/',
+    'https://agroball.diplom.nomoredomains.monster/',
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
