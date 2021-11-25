7833b9fbe6008eac2264e982d435a19a22290127
Roman TIMAKOV <timakov_r@M11highway.pro>
2021-11-25 15:36:32 +0300

    ef

--- middlewares/Validation.js
+++ middlewares/Validation.js
@@ -3,16 +3,16 @@ const { urlValidation } = require('../errors/CelebrateError');
 
 module.exports.registerValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
+    name: Joi.string().required().min(2).max(40),
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(4),
+    password: Joi.string().required().min(6),
   }),
 });
 
 module.exports.authValidation = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(4),
+    password: Joi.string().required().min(6),
   }),
 });
 
